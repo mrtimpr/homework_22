bd0ddf47126b95c9f86aadd59336c6f03bc93cd6
Timofey <mr.tim.iv@gmail.com>
2026-04-03 19:54:10 +0300

    updated project for homework 24

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'catalog'
@@ -6,4 +7,6 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('products/create/', views.product_create, name='product_create'),
 ]
