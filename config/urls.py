24177f0e10e62d0100cc2a68ef73cacd6988aada
Timofey <mr.tim.iv@gmail.com>
2026-04-14 00:08:25 +0300

    updated project for homework 24

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
-    path('blog/', include('blog.urls', namespace='blog')),
+    path('blogs/', include('blog.urls', namespace='blog')),
 ]
 
 if settings.DEBUG:
