47103decd9c6459ad37a4fb433103013e1b612a1
Timofey <mr.tim.iv@gmail.com>
2026-05-02 13:41:05 +0300

    updated project for homework 28

--- catalog/views.py
+++ catalog/views.py
@@ -72,7 +72,7 @@ class OwnerOrDeletePermissionRequiredMixin(LoginRequiredMixin, UserPassesTestMix
         return product.owner == user or user.has_perm('catalog.delete_product')
 
 
-class ProductUpdateView(OwnerRequiredMixin, UpdateView):
+class ProductUpdateView(OwnerOrDeletePermissionRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
