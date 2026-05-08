a81c20a7bd1ac3e6bf0d46d07f70e29870789ea5
Timofey <mr.tim.iv@gmail.com>
2026-05-09 00:29:36 +0300

    updated project for homework 29

--- config/settings.py
+++ config/settings.py
@@ -104,3 +104,18 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REDIS_URL = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/1')
+CACHE_TTL = int(os.getenv('CACHE_TTL', '900'))
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': REDIS_URL,
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
+        'KEY_PREFIX': 'homework_22',
+        'TIMEOUT': CACHE_TTL,
+    }
+}
